43d6baf4f1057d8f1a7c417d345f1bb4ea4ffc68
SMarkus27 <pereira.markus.27@hotmail.com>
2024-01-26 11:42:52 -0300

    test: product application and repository tests

---
+++ test/application/product/product.spec.ts
@@ -0,0 +1,120 @@
+import request from "supertest";
+import {app} from "../../../src/main";
+import mongoose, {connect} from "mongoose";
+import {ProductRepository} from "../../../src/infra/repository/products/repository";
+
+describe("Should create a product",  () => {
+
+    let mongoClient: mongoose.Mongoose;
+
+    beforeEach(async () => {
+        mongoClient = await connect(
+            "mongodb://localhost:27017",
+            {
+                dbName: "test"
+            });
+
+    });
+
+    afterAll(async () => {
+        jest.clearAllMocks();
+        await mongoose.connection.db.dropCollection("product")
+    });
+
+    it("should create product", async () => {
+        const product = {
+            title: "products 1",
+            description: "new products",
+            price: 20.55,
+            category: "new category",
+            owner_id: "123",
+            product_id: "456"
+        };
+        const response = await request(app).post("/api/v1/product").send(product);
+        const expectedResponse = { success: true, message: 'Product created' }
+
+        expect(response.body).toEqual(expectedResponse);
+    });
+
+    it("should not create product", async () => {
+        const product = {
+            title: "products 1",
+            description: "new products",
+            price: 20.55,
+            category: "new category",
+            owner_id: "123",
+            product_id: "456"
+        };
+        jest.spyOn(ProductRepository.prototype, "getOne").mockImplementation(async () => product )
+
+        const response = await request(app).post("/api/v1/product").send(product);
+        const expectedResponse = { success: true, message: 'Product already exist' }
+
+
+        expect(response.body).toEqual(expectedResponse);
+    });
+
+    it("should update a product", async () => {
+
+        const product = {
+            title: "products 1",
+            description: "new products",
+            price: 20.55,
+            category: "new category",
+            owner_id: "123",
+            product_id: "456"
+        };
+
+        const updateResult = {
+            modifiedCount:  1,
+            acknowledged: true,
+            matchedCount: 1,
+            upsertedCount: 0,
+            upsertedId: "1111111"
+        }
+
+        // @ts-ignore
+        jest.spyOn(ProductRepository.prototype, "update").mockImplementation( () => updateResult )
+
+        const response = await request(app).put(`/api/v1/product/${product.product_id}`).send(product);
+        const expectedResponse = { success: true, message: 'Product updated' }
+
+        expect(response.body).toEqual(expectedResponse);
+    });
+
+    it("should not update a product", async () => {
+
+        const product = {
+            title: "products 1",
+            description: "new products",
+            price: 20.55,
+            category: "new category",
+            owner_id: "123",
+            product_id: "456"
+        };
+
+        const updateResult = {
+            modifiedCount:  0,
+            acknowledged: true,
+            matchedCount: 1,
+            upsertedCount: 0,
+            upsertedId: "1111111"
+        }
+
+        // @ts-ignore
+        jest.spyOn(ProductRepository.prototype, "update").mockImplementation( () => updateResult )
+
+        const response = await request(app).put(`/api/v1/product/${product.product_id}`).send(product);
+        const expectedResponse = { success: true, message: 'Nothing to updated' }
+
+        expect(response.body).toEqual(expectedResponse);
+    });
+    it("should delete a product", async () => {
+        const product_id = "456"
+
+        const response = await request(app).delete(`/api/v1/product/${product_id}`).send();
+        const expectedResponse = { success: true, message: 'Product delete' }
+        expect(response.body).toEqual(expectedResponse);
+    });
+
+});
--- test/unit/domain/entties/products.spec.ts
+++ test/unit/domain/entties/products.spec.ts
@@ -5,52 +5,60 @@ describe("Product Entity tests", () => {
 
     it("should create a new Product", async () => {
         const product = {
-            title: "product 1",
-            description: "new product",
+            title: "products 1",
+            description: "new products",
             price: 20.55,
             category: "new category",
-            ownerId: "123"
+            owner_id: "123",
+            product_id: "456"
         };
 
         const newProduct = await Products.create(product);
-        expect(newProduct.title.getValue()).toEqual("product 1");
-        expect(newProduct.description.getValue()).toEqual("new product");
+        expect(newProduct.title.getValue()).toEqual("products 1");
+        expect(newProduct.description.getValue()).toEqual("new products");
         expect(newProduct.price.getValue()).toEqual(20.55);
         expect(newProduct.category).toEqual("new category");
-        expect(newProduct.ownerId).toEqual("123");
+        expect(newProduct.owner_id).toEqual("123");
+        expect(newProduct.product_id).toEqual("456");
 
     });
 
-    it("should not create a product with invalid title", async () => {
+    it("should not create a products with invalid title", async () => {
         const product = {
             title: "prod",
-            description: "new product",
+            description: "new products",
             price: 20.55,
             category: "new category",
-            ownerId: "123"
+            owner_id: "123",
+            product_id: "456"
+
         };
 
         await expect(async () => await Products.create(product)).rejects.toThrow("Invalid Title");
     })
-    it("should not create a product with invalid description", async () => {
+    it("should not create a products with invalid description", async () => {
         const product = {
-            title: "product 1",
+            title: "products 1",
             description: "new",
             price: 20.55,
             category: "new category",
-            ownerId: "123"
+            owner_id: "123",
+            product_id: "456"
+
         };
 
         await expect(async () => await Products.create(product)).rejects.toThrow("Invalid Description");
 
     })
-    it("should not create a product with invalid price", async () => {
+    it("should not create a products with invalid price", async () => {
         const product = {
-            title: "product 1",
-            description: "new product",
+            title: "products 1",
+            description: "new products",
             price: -1,
             category: "new category",
-            ownerId: "123"
+            owner_id: "123",
+            product_id: "456"
+
         };
 
         await expect(async () => await Products.create(product)).rejects.toThrow("Price must be more than zero");
